c3e62e40f28a7d65df1c4a3189c9736290b80405
Matteo <tognon1999@gmail.com>
2017-11-08 19:35:30 -0200

    Initial commit

---
+++ app/src/main/java/com/example/matteotognon/remedio/MenuEditarPerfil.java
@@ -0,0 +1,23 @@
+package com.example.matteotognon.remedio;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MatteoTognon on 07/11/2017.
+ */
+
+public class MenuEditarPerfil extends Fragment {
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_editar_perfil, container, false);
+        return myView;
+    }
+}
---
+++ app/src/main/java/com/example/matteotognon/remedio/MenuRemedios.java
@@ -0,0 +1,24 @@
+package com.example.matteotognon.remedio;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by MatteoTognon on 07/11/2017.
+ */
+
+public class MenuRemedios extends Fragment{
+    View myView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.activity_remedios, container, false);
+        return myView;
+    }
+}
+
